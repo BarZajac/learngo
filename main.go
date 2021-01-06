5130ae9b3ab7c98c462c4d2bead5dc78c84020ab
Bartek <bartek@bartek.dev>
2021-01-06 12:16:01 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "<html><h3>hello</h3></html>")
+	fmt.Println("HELLO")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+
+	// Create file server handler
+	//fs := http.FileServer( http.Dir( "/Users/Pawel/go/src/hello" ) )
+
+	// start HTTP server with `fs` as the default handler
+	//log.Fatal(http.ListenAndServe( ":8090", fs ))
+
+	log.Fatal(http.ListenAndServe(":8090", nil))
+}
