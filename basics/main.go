c8f0256c2bcf864307fb37c03c2a1cc138035992
Rafal Zajac <rzajac@gmail.com>
2021-01-08 21:48:22 +0100

    basic commit 1

---
+++ basics/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Variable declarations.
+	var i int
+	var f float64
+	var b byte
+	var s string
+
+	fmt.Printf("int     i: %d\n", i)
+	fmt.Printf("float64 f: %f\n", f)
+	fmt.Printf("byte    b: %d\n", b)
+	fmt.Printf("string  s: %s\n", s)
+
+}
